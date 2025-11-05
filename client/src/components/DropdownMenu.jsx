997eb681a53346e0fe241227a2b1fb8124fed6b4
Matthew Howard <matthewhoward@Matthews-MacBook-Air.local>
2025-11-05 14:42:31 +0100

    Reduced dropdown menu and gearlistdetailsmodal text spacing for mobile.

--- client/src/components/DropdownMenu.jsx
+++ client/src/components/DropdownMenu.jsx
@@ -33,7 +33,8 @@ export default function DropdownMenu({
         leaveTo="transform opacity-0 scale-95"
       >
         <Menu.Items
-          className={`absolute right-0 mt-2 ${menuWidth} bg-base-100 isolate shadow-lg ring-1 ring-black/5 focus:outline-none z-[1000] ${menuClassName}`}
+          className={`absolute right-0 mt-2 ${menuWidth} origin-top-right rounded-md bg-base-100 isolate shadow-lg ring-1 ring-black/5 focus:outline-none z-50
++                      max-h-[min(60dvh,24rem)] overflow-auto overscroll-contain ${menuClassName}`}
         >
           {items.map((item) =>
             item.render ? (
